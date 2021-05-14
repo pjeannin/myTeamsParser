ffa4ff1dd6c0d3d25ef98a158461f79be2744ad7
Pierre Jeannin <pierre.jeannin@epitech.eu>
2021-05-14 15:53:11 +0200

    almost end user loading

--- load_users.c
+++ load_users.c
@@ -47,7 +47,7 @@ user_infos_t **load_users()
     users_list_head->data = NULL;
     users_list_head->next = NULL;
     while (getline(&line, &size, file) != -1) {
-        linked_list_add_elem()
+        linked_list_add_elem(set_user_infos(split_string(line)));
     }
     return (user_infos);
 }
