baef4feec7a9186d27245f8a236e8d0bf087091c
Antony Fantapié <antony.fantapie@epitech.eu>
2021-05-14 14:21:57 +0200

    Finish saved_files

---
+++ cmake-build-default/CMakeFiles/parser.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/antonyftp/Documents/epitech-2.2nd-year/other/myTeamsParser/load_clients.c" "/Users/antonyftp/Documents/epitech-2.2nd-year/other/myTeamsParser/cmake-build-default/CMakeFiles/parser.dir/load_clients.c.o"
+  "/Users/antonyftp/Documents/epitech-2.2nd-year/other/myTeamsParser/main.c" "/Users/antonyftp/Documents/epitech-2.2nd-year/other/myTeamsParser/cmake-build-default/CMakeFiles/parser.dir/main.c.o"
+  "/Users/antonyftp/Documents/epitech-2.2nd-year/other/myTeamsParser/split_string.c" "/Users/antonyftp/Documents/epitech-2.2nd-year/other/myTeamsParser/cmake-build-default/CMakeFiles/parser.dir/split_string.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
