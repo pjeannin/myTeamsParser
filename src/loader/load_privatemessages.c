e32a223f5028a828400b04b5bffabf8dd008cefe
Antony Fantapié <antony.fantapie@epitech.eu>
2021-05-24 16:42:05 +0200

    Fix .gitignore

---
+++ src/loader/load_privatemessages.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** myTeams_Parser
+** File description:
+** Created by antonyftp
+*/
+
+#include "loader.h"
+
+private_message_list_t *load_privatemessages(user_t *users)
+{
+    private_message_list_t *private_messages_list_head = NULL;
+
+    return (private_messages_list_head);
+}
